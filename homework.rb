8d5ed06e32901317b7cd4c3d37334056fed5fa58
John <user@John-Pooleys-Macbook.local>
2020-01-13 16:50:08 +0000

    extension not done yet

--- homework.rb
+++ homework.rb
@@ -1,6 +1,6 @@
 class CohortStudent
 
-  def.initialize(input_student_name,input_cohort)
+  def initialize(input_student_name,input_cohort)
     @student_name = input_student_name
     @cohort = input_cohort
   end
@@ -15,7 +15,97 @@ class CohortStudent
     return @cohort
   end
 
+  # setters
 
+  def set_student_name(new_name)
+    @student_name = new_name
+  end
+
+  def set_cohort(new_cohort)
+    @cohort = new_cohort
+  end
+
+#  other stuff
+
+  def student_says()
+    return "#{@student_name} says 'Yeah boi!'"
+  end
+
+  def favourite_language(language)
+    return "#{@student_name} likes #{language}"
+  end
+
+
+
+end
+
+class SportsballTeam
+
+  attr_accessor :team_name,:players,:coach
+
+  def initialize(team_name,players,coach)
+    @points = 0
+    @team_name = team_name
+    @players = players
+    @coach = coach
+  end
+
+  def add_new_player(new_player)
+    @players << new_player
+  end
+
+  def find_player(player)
+
+    match = nil
+    for name in @players
+      if name == player
+        match = true
+      end
+    end
+    return match
+  end
+
+  def game_result(result)
+    if result == 'win'
+      @points += 1
+    else
+      return @points
+    end
+  end
+
+
+    # return true unless @players != player
+    # for name in @players
+    #   if name == player
+    #     return true
+    #   end
+    # end
+  # end
+
+
+
+  # #getters
+  # def get_team_name
+  #   return @team_name
+  # end
+  #
+  # def get_players
+  #   return @players
+  # end
+  #
+  # def get_coach
+  #   return @coach
+  # end
+
+
+
+
+  #setters
+
+
+
+
+  #other
 
 
 
--- specs/test_homework.rb
+++ specs/test_homework.rb
@@ -11,11 +11,84 @@ class TestHomework < Minitest::Test
   end
 
   def test_get_student_name
-    assert_equal('John', @student_name)
+    assert_equal('John', @cohort_student.get_student_name)
   end
 
   def test_get_cohort
-    assert_equal('G17', @cohort)
+    assert_equal('G17', @cohort_student.get_cohort)
   end
+
+  def test_set_student_name
+    @cohort_student.set_student_name('Not John')
+    assert_equal('Not John',@cohort_student.get_student_name)
+  end
+
+  def test_set_cohort
+    @cohort_student.set_cohort('3')
+    assert_equal('3', @cohort_student.get_cohort)
+  end
+
+  def test_student_says
+    @cohort_student.student_says
+    assert_equal("John says 'Yeah boi!'", @cohort_student.student_says)
+  end
+
+  def test_favourite_language
+    @cohort_student.favourite_language('English')
+    assert_equal("John likes English", @cohort_student.favourite_language('English'))
+  end
+
+
+
+
+
+
+
+
+end
+
+
+class Testhomework2 < Minitest::Test
+  def setup
+
+    @my_team = SportsballTeam.new("Teamsters",["mathew",'Mark','Luke','John'],"Skip")
+  end
+
+  def test_get_team_name
+    assert_equal("Teamsters", @my_team.team_name)
+  end
+
+  def test_get_players
+    assert_equal(["mathew",'Mark','Luke','John'], @my_team.players)
+  end
+
+  def test_get_coach
+    assert_equal("Skip", @my_team.coach)
+  end
+
+  def test_add_new_player
+    @my_team.add_new_player("Steve")
+    assert_equal(["mathew",'Mark','Luke','John', "Steve"], @my_team.players)
+  end
+
+  def test_find_player
+    assert_equal(true, @my_team.find_player('Mark'))
+  end
+
+  def test_find_player_false
+    assert_equal(nil, @my_team.find_player('Jesus') )
+  end
+
+  def test_game_result_win
+    puntos = @my_team.game_result('win')
+    assert_equal(puntos,1 )
+  end
+
+  def test_game_result_loss
+    puntos = @my_team.game_result('loss')
+    assert_equal(puntos, 0 )
+  end
+
+
 
 end
